f70dc86f462b9e6d73ba581236ead6ca36b274ac
Jeff_vvvv <jiefu.liang0225@gmail.com>
2018-06-11 11:51:15 +0000

    Front-end

--- index.js
+++ index.js
@@ -2,16 +2,31 @@ const express = require('express');
 const bodyParser = require("body-parser");
 const ejs = require('ejs');
 const http = require('http');
+
+//what we need to use for register/login/logout
+const cookieParser = require('cookie-parser');
+const validator = require('express-validator');
+const session = require('express-session');
+const mongoose = require('mongoose');
+const MongoStore = require('connect-mongo')(session);
+const flash = require('flash');
+const passport = require('passport')
+
+
+
 const container = require('./container');
 
 container.resolve(function(user){
     const app = SetupExpress();
+    mongoose.Promise = global.Promise;
+    mongoose.connect('mongodb://localhost/JeffChattingRoom');
     
+ 
     function SetupExpress(){
        const app = express();
        const server = http.createServer(app);
-       server.listen(process.env.PORT, process.env.IP, function(){
-           console.log('Server Start');
+       server.listen(3000, function(){
+           console.log('Listening on port 3000');
        });
        
        //ConfigureExpress
@@ -29,11 +44,28 @@ container.resolve(function(user){
     {
         //we can use all the resources in the file 'public'
         app.use(express.static('public'));
+        //add cookie-parser
+        app.use(cookieParser());
         //call ejs for front-end
         app.set('view engine', 'ejs');
         //configure body-parser
         app.use(bodyParser.json());
         app.use(bodyParser.urlencoded({extended: true}));
+        
+        //middleware
+        app.use(validator());
+        app.use(session({
+            secret: 'thisisasecretkey',
+            resave: true,
+            saveInitialized: true,
+            //set a database for our session storage
+            store: new MongoStore({mongooseConnection: mongoose.connection()})
+        }));
+        
+        app.use(flash());
+        
+        app.use(passport.initalize());
+        app.use(passport.session());
     }
     
 });
